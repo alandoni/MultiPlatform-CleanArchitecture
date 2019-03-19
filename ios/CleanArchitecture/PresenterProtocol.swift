d94797a3b07242b6743c41e12ec7a3b38188a2a2
Alan Quintiliano <aquintiliano@symphony.com>
2019-03-18 21:46:11 -0300

    Adding iOS target

---
+++ ios/CleanArchitecture/PresenterProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  PresenterProtocol.swift
+//  CleanArchitecture
+//
+//  Created by Alan Quintiliano on 18/03/19.
+//  Copyright © 2019 Alan Quintiliano. All rights reserved.
+//
+
+import Foundation
