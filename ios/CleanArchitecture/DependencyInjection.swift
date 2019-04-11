7d04e76fd38e3ce4124a0a519b4f6974901b213d
Alan Quintiliano <aquintiliano@symphony.com>
2019-04-11 17:58:25 -0300

    Adding Swinjection to iOS

---
+++ ios/CleanArchitecture/DependencyInjection.swift
@@ -0,0 +1,33 @@
+//
+//  DependencyInjection.swift
+//  CleanArchitecture
+//
+//  Created by Alan Quintiliano on 11/04/19.
+//  Copyright © 2019 Alan Quintiliano. All rights reserved.
+//
+
+import SwinjectStoryboard
+import main
+
+extension SwinjectStoryboard {
+    @objc class func setup() {
+        defaultContainer.register(Database.self) { _ in try! Database().initDatabase() }
+        defaultContainer.register(LoginPresenter.self) { r in
+            LoginPresenter(loginUseCase: r.resolve(LoginUseCase.self)!)
+        }
+        defaultContainer.register(LoginUseCase.self) { r in
+            LoginUseCase(localRepository: r.resolve(UserLocalRepository.self)!,
+                         remoteRepository: r.resolve(UserRemoteRepository.self)!)
+        }
+        defaultContainer.register(UserLocalRepository.self) { r in
+            UserLocalRepository(database: r.resolve(Database.self)!)
+        }
+        defaultContainer.register(UserRemoteRepository.self) { r in
+            UserRemoteRepository(request: HttpRequest())
+        }
+        defaultContainer.storyboardInitCompleted(LoginViewController.self) { r, c in
+            c.presenter = r.resolve(LoginPresenter.self)
+        }
+    }
+}
+
