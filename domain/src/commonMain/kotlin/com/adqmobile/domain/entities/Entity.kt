4d553bbaf35c61bd4d522739f47390e013344975
Alan Quintiliano <alan_doni@hotmail.com>
2019-04-11 19:13:31 -0300

    Removing Serializable from Entity

--- domain/src/commonMain/kotlin/com/adqmobile/domain/entities/Entity.kt
+++ domain/src/commonMain/kotlin/com/adqmobile/domain/entities/Entity.kt
@@ -1,15 +1,5 @@
 package com.adqmobile.domain.entities
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-
-@Serializable
 abstract class Entity {
     abstract override fun toString(): String
-
-    companion object {
-        fun fromJson(json: String): Entity {
-            return Json.parse(serializer(), json)
-        }
-    }
 }
