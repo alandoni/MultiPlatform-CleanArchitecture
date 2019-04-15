216c5c15e8e4db95d91302201619343986789198
Alan Quintiliano <aquintiliano@symphony.com>
2019-04-15 09:49:37 -0300

    Moving DI to another file

--- android/src/main/java/com/adqmobile/cleanarchitecture/App.kt
+++ android/src/main/java/com/adqmobile/cleanarchitecture/App.kt
@@ -1,29 +1,8 @@
 package com.adqmobile.cleanarchitecture
 
 import android.app.Application
-import com.adqmobile.cleanarchitecture.data.Database
-import com.adqmobile.cleanarchitecture.data.HttpRequest
-import com.adqmobile.presentation.LoginPresenter
-import com.adqmobile.domain.repositories.AbstractDatabase
-import com.adqmobile.domain.repositories.BaseRequest
-import com.adqmobile.data.user.UserLocalRepository
-import com.adqmobile.data.user.UserRemoteRepository
-import com.adqmobile.domain.usecases.LoginUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
-
-val ApplicationModule = module {
-    factory { Database(get()) as AbstractDatabase }
-    single { HttpRequest() as BaseRequest }
-}
-
-val LoginModule = module {
-    factory { LoginPresenter(get()) }
-    factory { UserLocalRepository(get()) }
-    factory { UserRemoteRepository(get()) }
-    factory { LoginUseCase(get(), get()) }
-}
 
 class App: Application() {
 
---
+++ android/src/main/java/com/adqmobile/cleanarchitecture/DependencyInjection.kt
@@ -0,0 +1,22 @@
+package com.adqmobile.cleanarchitecture
+
+import com.adqmobile.cleanarchitecture.data.Database
+import com.adqmobile.cleanarchitecture.data.HttpRequest
+import com.adqmobile.data.user.UserLocalRepository
+import com.adqmobile.data.user.UserRemoteRepository
+import com.adqmobile.domain.repositories.AbstractDatabase
+import com.adqmobile.domain.repositories.BaseRequest
+import com.adqmobile.domain.usecases.LoginUseCase
+import com.adqmobile.presentation.LoginPresenter
+import org.koin.dsl.module
+
+val ApplicationModule = module {
+    factory { Database(get()) as AbstractDatabase }
+    single { HttpRequest() as BaseRequest }
+}
+val LoginModule = module {
+    factory { LoginPresenter(get()) }
+    factory { UserLocalRepository(get()) }
+    factory { UserRemoteRepository(get()) }
+    factory { LoginUseCase(get(), get()) }
+}
